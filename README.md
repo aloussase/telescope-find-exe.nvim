16f4d122dcbefc70e1a75c52322d62f1497b3da7
Alexander Goussas <goussasalexander@gmail.com>
2025-06-22 00:37:51 -0500

    init

---
+++ README.md
@@ -0,0 +1,31 @@
+# Telescope-find-exe.nvim
+
+Find executable files in the current working directory (or others).
+
+## Installation
+
+### lazy.nvim
+
+```lua
+{
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+    { "nvim-lua/plenary.nvim" },
+    { "aloussase/telescope-find-exe.nvim" },
+  }
+}
+```
+
+## Setup 
+
+```lua
+require('telescope').load_extension('find_exe')
+```
+
+## Options
+
+You can choose in which directory to search:
+
+```lua
+Telescope find_exe dir=./build
+```
---
+++ lua/telescope/_extensions/find_exe.lua
@@ -0,0 +1,34 @@
+local pickers = require "telescope.pickers"
+local finders = require "telescope.finders"
+local conf = require("telescope.config").values
+local actions = require "telescope.actions"
+local action_state = require "telescope.actions.state"
+
+local function parse_opts(opts)
+  local o = opts or {}
+  o.dir = opts.dir or "."
+  return o
+end
+
+local function find_exe(opts)
+  opts = parse_opts(opts)
+  pickers.new(opts, {
+    prompt_title = "Find Executable Files in Current Directory",
+    finder = finders.new_oneshot_job({ "find", opts.dir, "-type", "f", "-executable" }, {}),
+    sorter = conf.generic_sorter(opts),
+    attach_mappings = function(prompt_bufnr)
+      actions.select_default:replace(function()
+        actions.close(prompt_bufnr)
+        local selection = action_state.get_selected_entry()
+        vim.fn.setreg("@", selection.value)
+      end)
+      return true
+    end,
+  }):find()
+end
+
+return require("telescope").register_extension {
+  exports = {
+    find_exe = find_exe,
+  },
+}
